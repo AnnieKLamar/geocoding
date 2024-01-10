87ee12237805cb3a0e47770ee94aaf0c868ced06
bcritt1 <bcritt@stanford.edu>
2024-01-10 12:26:47 -0800

    convert to batch process

--- wikification.sh
+++ wikification.sh
@@ -1,38 +1,34 @@
 #!/bin/sh
 
-while getopts f: flag
-do
-        case "${flag}" in
-                f) input_file=${OPTARG};;
-        esac
-done
+#while getopts f: flag
+#do
+#        case "${flag}" in
+#                f) input_file=${OPTARG};;
+#        esac
+#done
 
-ml purge
-ml java
+#ml purge
+#ml java
 echo -e "\nRemoving current installation of Stanford CoreNLP... \n"
-rm -r stanford-corenlp*
+rm -r $GROUP_HOME/$USER/stanford-corenlp*
 
 echo -e "\Installing Stanford CoreNLP... \n"
-wget https://nlp.stanford.edu/software/stanford-corenlp-4.5.5.zip
-unzip stanford-corenlp-4.5.5.zip
-rm stanford-corenlp-4.5.5/input.txt stanford-corenlp-4.5.5/input.txt.xml
+wget https://nlp.stanford.edu/software/stanford-corenlp-4.5.5.zip -P $GROUP_HOME/$USER
+unzip $GROUP_HOME/$USER/stanford-corenlp-4.5.5.zip -d $GROUP_HOME/$USER
 
-
-cp $input_file stanford-corenlp-4.5.5/input.txt
-cd stanford-corenlp-4.5.5
+cd $GROUP_HOME/$USER/stanford-corenlp-4.5.5
 echo -e "\nCollecting extra models for wikification... \n"
-wget https://huggingface.co/stanfordnlp/corenlp-english-kbp/resolve/main/stanford-corenlp-models-english-kbp.jar https://huggingface.co/stanfordnlp/corenlp-english-extra/resolve/main/stanford-corenlp-models-english-extra.jar
+wget https://huggingface.co/stanfordnlp/corenlp-english-kbp/resolve/main/stanford-corenlp-models-english-kbp.jar https://huggingface.co/stanfordnlp/corenlp-english-extra/resolve/main/stanford-corenlp-models-english-extra.jar -P $GROUP_HOME/$USER/stanford-corenlp-4.5.5/
 
-unzip -o stanford-corenlp-models-english-extra.jar
-unzip -o stanford-corenlp-models-english-extra.jar
-unzip -o stanford-corenlp-models-english-kbp.jar
+unzip -o $GROUP_HOME/$USER/stanford-corenlp-4.5.5/stanford-corenlp-models-english-extra.jar -d $GROUP_HOME/$USER/stanford-corenlp.4.5.5/
+unzip -o $GROUP_HOME/$USER/stanford-corenlp-4.5.5/stanford-corenlp-models-english-kbp.jar -d $GROUP_HOME/$USER/stanford-corenlp.4.5.5/
 
-zip stanford-corenlp-4.5.5-models.jar stanford-corenlp-4.5.5-models.jar stanford-corenlp-models-english-extra.jar stanford-corenlp-models-english-kbp.jar
+zip $GROUP_HOME/$USER/stanford-corenlp.4.5.5/stanford-corenlp-4.5.5-models.jar $GROUP_HOME/$USER/stanford-corenlp.4.5.5/stanford-corenlp-4.5.5-models.jar $GROUP_HOME/$USER/stanford-corenlp.4.5.5/stanford-corenlp-models-english-extra.jar $GROUP_HOME/$USER/stanford-corenlp.4.5.5/stanford-corenlp-models-english-kbp.jar
 echo -e "\nEstablishing classpath... \n"
 for file in `find . -name "*.jar"`; do export
 CLASSPATH="$CLASSPATH:`realpath $file`"; done
 echo -e "\nWikifying text... \n"
 
-ls -d -1 /path/to/txt/corpus/*.* > filelist.lst
-mkdir /scratch/users/$USER/output/coreEntities
-java -cp "*" -Xmx16g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,pos,lemma,ner,entitylink -filelist "filelist.lst" -outputDirectory "/scratch/users/$USER/output/coreEntities/"
+ls -d -1 /scratch/users/$USER/testCorpus/*.* > filelist.lst
+mkdir -p /scratch/users/$USER/outputs/coreEntities
+java -cp "*" -Xmx16g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,pos,lemma,ner,entitylink -filelist "filelist.lst" -outputDirectory "/scratch/users/$USER/outputs/coreEntities/"
